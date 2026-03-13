b3dbf49856b588b7b8d1db709233485fa7010ff4
Staf <staf.logie@ugent.be>
2026-03-13 17:51:14 +0100

    arduino

--- arduino/test_outputs/test_outputs.ino
+++ arduino/test_outputs/test_outputs.ino
@@ -2,11 +2,13 @@ int ledPin = 3;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);  
   delay(1000);                      
   digitalWrite(ledPin, LOW);   
-  delay(1000);                      
+  delay(1000);      
+  Serial.println(“hello”);                
 }
